d3501a74bc9c25d9497a9332fe441cba142a8c48
hariza17 <locoariza17@gmail.com>
2016-08-18 16:50:12 -0500

    211

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -15,7 +15,7 @@ class PagesController extends Controller
      */
     public function index()
     {
-       	$apiUrl='http://localhost:8000/api/';
+       	$apiUrl=env('URL_API');
     	return view('index')->with('apiUrl', $apiUrl);
 
     }
