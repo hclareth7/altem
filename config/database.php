c5039d3f2f1a5c4d3b31c81f91e350b47a47c644
hariza17 <locoariza17@gmail.com>
2016-12-02 15:37:28 -0500

    Oracle conect3

--- config/database.php
+++ config/database.php
@@ -59,12 +59,12 @@ return [
 
         'sirius' => [
             'driver'    => 'oci8',
-            'tns'       => env('DB_TNS', ''),
-            'host'      => env('DB_HOST', ''),
-            'port'      => env('DB_PORT', '1521'),
-            'database'  => env('DB_DATABASE', ''),
-            'username'  => env('DB_USERNAME', ''),
-            'password'  => env('DB_PASSWORD', ''),
+            'tns'       =>  '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.8.10)(PORT = 1521))(CONNECT_DATA =(SID = PROD)))',
+            'host'      =>  '172.16.8.10',
+            'port'      =>  '1521',
+            'database'  =>  'PROD',
+            'username'  =>'altem',
+            'password'  =>  'Altem_2016',
             'charset'   => 'WE8ISO8859P1',
             'prefix'    => '',
             'quoting'   => false,
--- config/oracledb.php
+++ config/oracledb.php
@@ -3,12 +3,12 @@
 return [
     'oracle' => [
         'driver'    => 'oci8',
-        'tns'       => env('DB_TNS', '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.8.10)(PORT = 1521))(CONNECT_DATA =(SID = PROD)))'),
-        'host'      => env('DB_HOST', '172.16.8.10'),
-        'port'      => env('DB_PORT', '1521'),
-        'database'  => env('DB_DATABASE', 'PROD'),
-        'username'  => env('DB_USERNAME', 'altem'),
-        'password'  => env('DB_PASSWORD', 'Altem_2016'),
+        'tns'       =>  '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.8.10)(PORT = 1521))(CONNECT_DATA =(SID = PROD)))',
+        'host'      =>  '172.16.8.10',
+        'port'      =>  '1521',
+        'database'  =>  'PROD',
+        'username'  =>'altem',
+        'password'  =>  'Altem_2016',
         'charset'   => 'WE8ISO8859P1',
         'prefix'    => '',
         'quoting'   => false,
