21d77e6bb880afe19079f1190f197537e15e954a
hariza17 <locoariza17@gmail.com>
2016-12-02 14:02:31 -0500

    Oracle conect2

--- config/oracledb.php
+++ config/oracledb.php
@@ -3,7 +3,7 @@
 return [
     'oracle' => [
         'driver'    => 'oci8',
-        'tns'       => env('DB_TNS', ''),
+        'tns'       => env('DB_TNS', '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.8.10)(PORT = 1521))(CONNECT_DATA =(SID = PROD)))'),
         'host'      => env('DB_HOST', '172.16.8.10'),
         'port'      => env('DB_PORT', '1521'),
         'database'  => env('DB_DATABASE', 'PROD'),
